0e7ac10fa12ae6ca89feb50545368b57b5b2ca61
lynda <l_bounif@esi.dz>
2020-10-26 13:22:21 +0100

    add http api

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 // server.js
 import express from 'express'
+import redisClient from './redis-client'
 
 const app = express()
 
+
+app.get('/store/:key', async (req, res) => {
+    const { key } = req.params
+    const value = req.query
+    await redisClient.setAsync(key, JSON.stringify(value))
+    return res.send('Success')
+})
+
+app.get('/:key', async (req, res) => {
+    const { key } = req.params
+    const rawData = await redisClient.getAsync(key)
+    return res.json(JSON.parse(rawData))
+})
+
 app.get('/', (req, res) => {
     return res.send('Hello world')
-});
+})
+
+
 
 const PORT = process.env.PORT || 3000
 
